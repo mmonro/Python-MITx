222b495f62e057a8ed65d19f507ec7f1836f2aed
mmonro <monte.monroe@hotmail.com>
2016-02-05 10:35:49 -0700

    Create howMany.py

---
+++ Lesson6/howMany.py
@@ -0,0 +1,18 @@
+animals = { 'a': ['aardvark'], 'b': ['baboon'], 'c': ['coati']}
+
+animals['d'] = ['donkey']
+animals['d'].append('dog')
+animals['d'].append('dingo')
+
+def howMany(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+
+    returns: int, how many values are in the dictionary.
+    '''
+    result = 0
+    for value in aDict.values():
+        result += len(value)
+    return result
+    
+print howMany(animals)
