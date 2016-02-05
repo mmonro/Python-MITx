ecbe0608ac169443092dcdcca03a3a3fa4a8a418
mmonro <monte.monroe@hotmail.com>
2016-02-05 10:22:22 -0700

    Create counting_bobs.py

---
+++ Problem_Set1/counting_bobs.py
@@ -0,0 +1,14 @@
+s = 'azcbobobegghakl'
+
+count = 0
+index = 0
+
+while True:
+    index = s.find('bob', index)
+    if index >= 0:
+        count += 1
+        index += 1
+    else:
+        break
+
+print ('Number of times bob occurs is: ' + str(count))
