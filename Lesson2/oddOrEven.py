1e8cf5ef01cfadace3be62fba06c5dfa6174845d
mmonro <monte.monroe@hotmail.com>
2016-02-05 11:00:42 -0700

    Create oddOrEven.py

---
+++ Lesson2/oddOrEven.py
@@ -0,0 +1,9 @@
+x = int(raw_input('Please enter an integer:  '))
+if x%2 == 0:
+    print (' ')
+    print ('Even')
+else:
+    print (' ')
+    print ('Odd')
+
+print ('Done with this conditional example')
