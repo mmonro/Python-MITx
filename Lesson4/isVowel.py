3ede4603f0d626e502f1aa640c4a1a77db553e4a
mmonro <monte.monroe@hotmail.com>
2016-02-05 10:51:39 -0700

    Create isVowel.py

---
+++ Lesson4/isVowel.py
@@ -0,0 +1,7 @@
+def isVowel(char):
+    '''
+    char: a single letter of any case
+
+    returns: True if char is a vowel and False otherwise.
+    '''
+    return bool(char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u' or char == 'A' or char == 'E' or char == 'I' or char == 'O' or char == 'U')
